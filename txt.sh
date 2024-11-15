1dce1c6b2bd5fa955bc8adf595416cd2ff3ef3ba
毛尧堂 <601209524@qq.com>
2024-11-15 15:59:43 +0800

    first commit

--- txt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 假设所有的 .scel 文件都在当前目录下
-for file in *.scel; do
-    # 提取文件名（不含扩展名）
-    filename=$(basename "$file" .scel)
-    # 调用 Python 脚本进行转换
-    scel2org5 "$file" -o "${filename}.txt"
-done
